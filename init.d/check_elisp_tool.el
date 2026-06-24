f7336a3f28f66e7beef9bede939772a85ae5e11d
Ignacio Randazzo <ignacio@randazzo.ar>
2026-06-24 12:28:23 -0300

    Added license to source files

--- init.d/check_elisp_tool.el
+++ init.d/check_elisp_tool.el
@@ -1,5 +1,22 @@
 ;; -*- lexical-binding: t; -*-
 
+;; Emacboros --- Agent orchestration in Emacs
+;; Copyright (C) 2026 Ignacio Agustín Randazzo
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 ;;; Elisp Syntax Checker Tool for gptel
 ;; Provides a tool that checks .el files for syntax errors, unbalanced
 ;; parentheses, and byte-compilation warnings -- all without modifying
